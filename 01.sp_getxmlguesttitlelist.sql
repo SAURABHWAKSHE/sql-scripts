bb08a4ff4af4f7c2752182f7015a6b1d49bd76be
SAURABHWAKSHE <32311169+SAURABHWAKSHE@users.noreply.github.com>
2019-04-22 16:20:00 +0530

    Update 01.sp_getxmlguesttitlelist.sql

--- 01.sp_getxmlguesttitlelist.sql
+++ 01.sp_getxmlguesttitlelist.sql
@@ -4,6 +4,8 @@ go
 drop procedure sp_xml_guesttitlelist
 go
 create procedure sp_xml_guesttitlelist
+
+
 as
 begin 
 	select t.TitleCode, t.TitleName from TitleMast t (nolock) where Active=1 and isnull(t.TitleForXML,0)=1 order by t.orderno
